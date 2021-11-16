033c8c021eebd5f2045eca5dbc39ee81f7869d38
Piotr Grzywa <pgrzywa@vuestorefront.io>
2021-11-16 15:22:56 +0100

    fix(os-203): add changelog and update docs

--- packages/core/core/src/utils/helpers/index.ts
+++ packages/core/core/src/utils/helpers/index.ts
@@ -1,6 +1,11 @@
 import { useRouter } from '@nuxtjs/composition-api';
 
-const addBasePath = (path: string): string => {
+/**
+ * Adds prefix with base path configured in router.base to provided url
+ * @param {string} path - url to which base path will be added
+ * @returns Relative path prefixed with router.base or not modified absolute path (it needs start from http or https)
+ */
+function addBasePath (path: string): string {
   const pattern = /^((http|https):\/\/)/;
 
   if (pattern.test(path)) {
@@ -9,7 +14,7 @@ const addBasePath = (path: string): string => {
 
   const basePath = (useRouter().options.base).slice(0, -1);
   return `${basePath}${path}`;
-};
+}
 
 export {
   addBasePath
