3ec54c1097c9f8cdddd4911cb0cb1a973321506d
jason.wolf <jason.wolf@borngroup.com>
2022-01-11 08:41:37 -0500

    #6602 refactored rollup * Added type annotation at line 3 to support static analysis and IDE auto-complete * Added commonjs dependency per rollup guidance * Added progress bar / output * Added nodePolyfills to augment build process * Added madge to support console & error messaging * Refactored build configurations to be autonomous vs depending on a global config

--- packages/core/core/rollup.config.js
+++ packages/core/core/rollup.config.js
@@ -1,40 +1,130 @@
-import pkg from './package.json';
+// core/core/rollup.js
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import replace from '@rollup/plugin-replace';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import minimist from 'minimist';
+import progress from 'rollup-plugin-progress';
+import ttypescript from 'ttypescript';
+import babel from '@rollup/plugin-babel';
+import madge from 'madge';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
+import pkg from './package.json';
 
-export function generateBaseConfig(pkg) {
-  return {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: pkg.main,
-        format: 'cjs',
-        sourcemap: true
-      },
-      {
-        file: pkg.module,
-        format: 'es',
-        sourcemap: true
-      }
-    ],
-    external: [
-      ...Object.keys(pkg.dependencies || {})
-    ],
+const argv = minimist(process.argv.slice(2));
+
+// Refer to https://rollupjs.org/guide/en#output-globals for details
+const globals = {
+  // Provide global variable names to replace your external imports
+  vue: 'Vue',
+  axios: 'axios',
+  'is-https': 'isHttps',
+  consola: 'consola'
+};
+
+// function detects circular dependencies & dependency warnings and prints them to console
+madge('./src/index.ts', {}).then((res) => {
+  // eslint-disable-next-line no-console
+  console.log(res.warnings()), console.log(res.circular());
+});
+
+// sets the base / common config used across all output formats
+const baseConfig = {
+  input: 'src/index.ts',
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+    '@nuxtjs/composition-api'
+  ],
+  plugins: {
+    // https://github.com/rollup/plugins/tree/master/packages/node-resolve
+    replace: {
+      preventAssignment: true,
+      'process.env.NODE_ENV': JSON.stringify('development')
+    },
+
+    nodeResolve: {
+      isRequire: false,
+      preferBuiltins: true,
+      moduleDirectories: ['node_modules']
+    },
+    progress: {
+      clearLine: true // set to false to print / retain the output in console
+    },
+    // typescript options are inherited from tsconfig
+    nodePolyfills: {
+      include: [
+        'node_modules',
+        'src'
+      ],
+      sourceMap: true
+    },
+    typescript: {
+      typescript: ttypescript,
+      check: true, // runs diagnostics on the code
+      verbosity: 1,
+      clean: true, // generates a clean build each time
+      abortOnError: true,
+      include: ['*.ts+(|x)', '**/*.ts+(|x)']
+    },
+    babel: {
+      extensions: ['.ts', '.tsx'],
+      exclude: 'node_modules/**',
+      babelHelpers: 'bundled',
+      skipPreflightCheck: true // resolves compilation failure with use of runtime for helpers
+    }
+  }
+};
+// Customize configs for individual targets /output formats
+const buildFormats = [];
+if (!argv.format || argv.format === 'es') {
+  const esConfig = {
+    ...baseConfig,
+    output: {
+      file: pkg.module, // sets naming convention from package.json
+      format: 'es',
+      globals,
+      sourcemap: true
+    },
     plugins: [
-      typescript({
-        // eslint-disable-next-line global-require
-        typescript: require('typescript'),
-        objectHashIgnoreUnknownHack: false
-      }),
-      replace({
-        __DEV__: process.env.NODE_ENV === 'development',
-        delimiters: ['', ''],
-        preventAssignment: true
-      })
+      progress(baseConfig.plugins.progress),
+      nodeResolve(baseConfig.plugins.nodeResolve),
+      nodePolyfills(baseConfig.plugins.nodePolyfills),
+      replace(baseConfig.plugins.replace),
+      typescript(baseConfig.plugins.typescript),
+      babel(baseConfig.plugins.babel)
     ]
   };
+  buildFormats.push(esConfig);
 }
 
-const baseConfig = generateBaseConfig(pkg);
+if (!argv.format || argv.format === 'cjs') {
+  const umdConfig = {
+    ...baseConfig,
+    output: {
+      exports: 'auto',
+      file: pkg.main,
+      format: 'cjs',
+      globals,
+      sourcemap: true
+    },
+    plugins: [
+      nodeResolve(baseConfig.plugins.nodeResolve),
+      progress(baseConfig.plugins.progress),
+      replace(baseConfig.plugins.replace),
+      commonjs({
+        include: /node_modules/
+      }),
+      nodePolyfills(baseConfig.plugins.nodePolyfills),
+      typescript(baseConfig.plugins.typescript),
+      babel(baseConfig.plugins.babel)
+    ]
+  };
+  buildFormats.push(umdConfig);
+}
 
-export default baseConfig;
+export default buildFormats;
